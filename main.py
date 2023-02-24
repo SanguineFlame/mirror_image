6a2337fe747a2c4b4e9187c5071aa997066cf670
SanguineFlame <nickmaggeri@gmail.com>
2023-02-24 12:00:21 -0500

    Added SQLITE database functionality to store genre tallys of the music I listened to.

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from spotipy.oauth2 import SpotifyOAuth
 import collections
 import json
 import pandas
+import sqlite3
 
-scope = 'user-read-recently-played'
+conn = sqlite3.connect('python_spotify.sqlite')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS Genres(genre_tally, TEXT, Tally Integer)''')
 
 sp_obj = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.client_id, client_secret=secrets.client_secret,
-                                                   redirect_uri=secrets.redirect_uri, scope=scope))
+                                                   redirect_uri=secrets.redirect_uri,
+                                                   scope='user-read-recently-played'))
 
 recently_played = sp_obj.current_user_recently_played(limit=50)
 
@@ -24,6 +29,7 @@ tally = collections.Counter()
 for genre in collection_of_genres:
     tally[genre] += 1
 
-genre_df = pandas.DataFrame.from_dict(tally, orient='index', columns=['Genre Tally'])
-print(genre_df.to_string())
+genre_df = pandas.DataFrame(list(tally.items()), columns=['Genre', 'Tally'])
+genre_df.to_sql(name='genre_tally', con=conn, if_exists='replace', index=False)
+
 
